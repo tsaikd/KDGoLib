902c0a0f423a7cdf624ff4cb1030b4c98e4ab20c
tsaikd <tsaikd@gmail.com>
2014-09-06 19:07:11 +0800

    add env.GetBool()

--- env/env.go
+++ env/env.go
@@ -4,9 +4,23 @@ import (
 	"os"
 )
 
+// like os.Getenv and provide default value
 func GetString(key string, defvalue string) (value string) {
 	value = os.Getenv(key)
 	if value == "" {
+		value = defvalue
+	}
+	return value
+}
+
+func GetBool(key string, defvalue bool) (value bool) {
+	envValue := os.Getenv(key)
+	switch envValue {
+	case "true", "True", "TRUE", "yes", "Yes", "YES", "1":
+		value = true
+	case "false", "False", "FALSE", "no", "No", "NO", "0":
+		value = false
+	default:
 		value = defvalue
 	}
 	return value
--- env/env_test.go
+++ env/env_test.go
@@ -7,12 +7,39 @@ import (
 )
 
 func Test_GetString(t *testing.T) {
+	var value string
 	assert := assert.New(t)
 
-	value := GetString("IMPOSSIBLE_ENV_KEY", "Impossible value !")
+	value = GetString("IMPOSSIBLE_ENV_KEY", "Impossible value !")
 	assert.Equal("Impossible value !", value, "Get ENV 'IMPOSSIBLE_ENV_KEY'")
 
 	path := os.Getenv("PATH")
 	value = GetString("PATH", "")
 	assert.Equal(path, value, "Get ENV 'PATH'")
+}
+
+func Test_GetBool(t *testing.T) {
+	var value bool
+	assert := assert.New(t)
+
+	value = GetBool("IMPOSSIBLE_ENV_KEY", true)
+	assert.Equal(true, value, "Get ENV 'IMPOSSIBLE_ENV_KEY'")
+
+	value = GetBool("IMPOSSIBLE_ENV_KEY", false)
+	assert.Equal(false, value, "Get ENV 'IMPOSSIBLE_ENV_KEY'")
+
+	os.Setenv("IMPOSSIBLE_ENV_KEY_TRUE", "true")
+	value = GetBool("IMPOSSIBLE_ENV_KEY_TRUE", true)
+	assert.Equal(true, value, "Get ENV 'IMPOSSIBLE_ENV_KEY_TRUE'")
+
+	value = GetBool("IMPOSSIBLE_ENV_KEY_TRUE", false)
+	assert.Equal(true, value, "Get ENV 'IMPOSSIBLE_ENV_KEY_TRUE'")
+
+	os.Setenv("IMPOSSIBLE_ENV_KEY_FALSE", "false")
+	value = GetBool("IMPOSSIBLE_ENV_KEY_FALSE", true)
+	assert.Equal(false, value, "Get ENV 'IMPOSSIBLE_ENV_KEY_FALSE'")
+
+	value = GetBool("IMPOSSIBLE_ENV_KEY_FALSE", false)
+	assert.Equal(false, value, "Get ENV 'IMPOSSIBLE_ENV_KEY_FALSE'")
+
 }
