d575ae56268092919e2c574aacbdbad2c5e9b850
tsaikd <tsaikd@gmail.com>
2015-05-06 02:28:07 +0800

    add MustTemplate
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- must/must.go
+++ must/must.go
@@ -1,8 +1,19 @@
 package must
 
+import (
+	"text/template"
+)
+
 func MustString(text string, err error) string {
 	if err != nil {
 		panic(err)
 	}
 	return text
+}
+
+func MustTemplate(tmpl *template.Template, err error) *template.Template {
+	if err != nil {
+		panic(err)
+	}
+	return tmpl
 }
