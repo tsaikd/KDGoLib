48f0f9e1a3644e4cd42937d716dabefe82c1c2a9
tsaikd <tsaikd@gmail.com>
2015-06-05 10:51:09 +0800

    add httputil.PingIgnoreCertificate
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- httputil/httputil.go
+++ httputil/httputil.go
@@ -1,6 +1,8 @@
 package httputil
 
 import (
+	"crypto/tls"
+	"errors"
 	"io/ioutil"
 	"net/http"
 )
@@ -16,5 +18,22 @@ func ReadResponse(resp *http.Response) (body string, err error) {
 	}
 
 	body = string(data)
+	return
+}
+
+func PingIgnoreCertificate(surl string) (err error) {
+	var (
+		resp *http.Response
+	)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	if resp, err = client.Get(surl); err != nil {
+		return
+	}
+	if resp.StatusCode > 200 || resp.StatusCode >= 400 {
+		return errors.New("return unexpect status code")
+	}
 	return
 }
