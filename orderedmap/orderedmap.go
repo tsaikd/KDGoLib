0cabaee640fed579c4b89fbbe3c50a85e96a7b2f
tsaikd <tsaikd@gmail.com>
2015-06-14 14:01:18 +0800

    orderedmap support unmarshal
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- orderedmap/orderedmap.go
+++ orderedmap/orderedmap.go
@@ -1,34 +1,33 @@
 package orderedmap
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
-type OrderedMap interface {
-	Add(v interface{}) bool
-	Remove(v interface{}) bool
-	IsEmpty() bool
-	Exist(v interface{}) bool
-	Map() map[interface{}]bool
-	Slice() []interface{}
-	MarshalJSON() ([]byte, error)
-}
-
-type OrderedMapData struct {
+type OrderedMap struct {
 	datamap   map[interface{}]bool
 	dataslice []interface{}
 }
 
 func New(vs ...interface{}) (t OrderedMap) {
-	t = &OrderedMapData{
-		datamap:   make(map[interface{}]bool),
-		dataslice: []interface{}{},
-	}
+	t.ensureDataStruct()
 	for _, v := range vs {
 		t.Add(v)
 	}
 	return
 }
 
-func (t *OrderedMapData) Add(v interface{}) bool {
+func (t *OrderedMap) ensureDataStruct() {
+	if t.datamap == nil {
+		t.datamap = make(map[interface{}]bool)
+	}
+	if t.dataslice == nil {
+		t.dataslice = []interface{}{}
+	}
+}
+
+func (t *OrderedMap) Add(v interface{}) bool {
+	t.ensureDataStruct()
 	_, ok := t.datamap[v]
 	if ok {
 		return false
@@ -39,7 +38,8 @@ func (t *OrderedMapData) Add(v interface{}) bool {
 	}
 }
 
-func (t *OrderedMapData) Remove(v interface{}) bool {
+func (t *OrderedMap) Remove(v interface{}) bool {
+	t.ensureDataStruct()
 	_, ok := t.datamap[v]
 	if !ok {
 		return false
@@ -54,23 +54,49 @@ func (t *OrderedMapData) Remove(v interface{}) bool {
 	return true
 }
 
-func (t OrderedMapData) IsEmpty() bool {
+func (t OrderedMap) IsEmpty() bool {
+	t.ensureDataStruct()
 	return len(t.dataslice) < 1
 }
 
-func (t OrderedMapData) Exist(v interface{}) bool {
+func (t OrderedMap) Exist(v interface{}) bool {
+	t.ensureDataStruct()
 	_, exist := t.datamap[v]
 	return exist
 }
 
-func (t OrderedMapData) Map() map[interface{}]bool {
+func (t OrderedMap) Map() map[interface{}]bool {
+	t.ensureDataStruct()
 	return t.datamap
 }
 
-func (t OrderedMapData) Slice() []interface{} {
+func (t OrderedMap) Slice() []interface{} {
+	t.ensureDataStruct()
 	return t.dataslice
 }
 
-func (t OrderedMapData) MarshalJSON() ([]byte, error) {
+func (t OrderedMap) MarshalJSON() ([]byte, error) {
+	t.ensureDataStruct()
 	return json.Marshal(t.Slice())
+}
+
+func (t *OrderedMap) UnmarshalJSON(data []byte) (err error) {
+	var (
+		dataslice []interface{}
+	)
+
+	t.ensureDataStruct()
+
+	if err = json.Unmarshal(data, &dataslice); err != nil {
+		return
+	}
+
+	t.datamap = map[interface{}]bool{}
+	t.dataslice = []interface{}{}
+
+	for _, v := range dataslice {
+		t.Add(v)
+	}
+
+	return
 }
