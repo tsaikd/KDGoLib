55fee882ccd297336f9f180dc0379160ebe5fd70
tsaikd <tsaikd@gmail.com>
2015-11-25 04:36:30 +0800

    add logutil.DefaultLogger
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- logutil/logutil.go
+++ logutil/logutil.go
@@ -1,5 +1,25 @@
 package logutil
 
+import (
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/inject"
+	"github.com/tsaikd/KDGoLib/logrusutil"
+)
+
+var (
+	timestampFormat = "2006/01/02 15:04:05"
+	DefaultLogger   = &logrus.Logger{
+		Out: os.Stdout,
+		Formatter: &logrusutil.ConsoleLogFormatter{
+			TimestampFormat: timestampFormat,
+		},
+		Hooks: make(logrus.LevelHooks),
+		Level: logrus.InfoLevel,
+	}
+)
+
 type StdLogger interface {
 	Print(...interface{})
 	Printf(string, ...interface{})
@@ -30,4 +50,22 @@ type LevelLogger interface {
 	Error(...interface{})
 	Errorf(string, ...interface{})
 	Errorln(...interface{})
+}
+
+// GetStdLogger get StdLogger instance from injector
+func GetStdLogger(inj inject.Injector) StdLogger {
+	if loggerType := inj.Get(inject.InterfaceOf((*StdLogger)(nil))); loggerType.IsValid() {
+		logger := loggerType.Interface().(StdLogger)
+		return logger
+	}
+	return DefaultLogger
+}
+
+// GetLevelLogger get LevelLogger instance from injector
+func GetLevelLogger(inj inject.Injector) LevelLogger {
+	if loggerType := inj.Get(inject.InterfaceOf((*LevelLogger)(nil))); loggerType.IsValid() {
+		logger := loggerType.Interface().(LevelLogger)
+		return logger
+	}
+	return DefaultLogger
 }
