3357d89f85d32febe2fddb7c7d578a7b8978ebb4
tsaikd <tsaikd@gmail.com>
2016-01-03 17:00:54 +0800

    add injectutil.CheckErrorValues()
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- injectutil/injectutil.go
+++ injectutil/injectutil.go
@@ -6,20 +6,25 @@ import (
 	"github.com/codegangsta/inject"
 )
 
-// Invoke and handle return value for error type
-func Invoke(inj inject.Injector, f interface{}) (refvs []reflect.Value, err error) {
-	if refvs, err = inj.Invoke(f); err != nil {
-		return
-	}
+// CheckErrorValues return error if refvs contains error
+func CheckErrorValues(refvs []reflect.Value) (err error) {
 	for _, refv := range refvs {
 		if refv.IsValid() {
 			refvi := refv.Interface()
 			switch refvi.(type) {
 			case error:
-				err = refvi.(error)
-				return
+				return refvi.(error)
 			}
 		}
 	}
+	return
+}
+
+// Invoke and handle return value for error type
+func Invoke(inj inject.Injector, f interface{}) (refvs []reflect.Value, err error) {
+	if refvs, err = inj.Invoke(f); err != nil {
+		return
+	}
+	err = CheckErrorValues(refvs)
 	return
 }
