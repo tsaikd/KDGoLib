f49f3ab313929b05f3d7bb244a69f6017c1b4a66
tsaikd <tsaikd@gmail.com>
2016-06-09 17:45:00 +0800

    add futil.IsDir()
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- futil/futil.go
+++ futil/futil.go
@@ -2,12 +2,33 @@ package futil
 
 import (
 	"os"
+
+	"github.com/tsaikd/KDGoLib/errutil"
 )
 
+// IsExist return true if path exist
 func IsExist(path string) bool {
 	_, err := os.Stat(path)
 	if err == nil {
 		return true
 	}
 	return os.IsExist(err)
+}
+
+// IsDir return true if path exist and is directory
+func IsDir(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer func() {
+		errutil.Trace(f.Close())
+	}()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return false
+	}
+
+	return fi.Mode().IsDir()
 }
