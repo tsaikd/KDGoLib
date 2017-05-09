3fc7ff8c2fa8bc98cdfecb550ea67c8dd6fc8438
tsaikd <tsaikd@gmail.com>
2017-05-09 16:30:27 +0800

    sqlutil/dbtypes: add context funcs
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- sqlutil/dbtypes/interface.go
+++ sqlutil/dbtypes/interface.go
@@ -1,6 +1,7 @@
 package dbtypes
 
 import (
+	"context"
 	"database/sql"
 	"database/sql/driver"
 
@@ -20,26 +21,32 @@ type Result interface {
 // Exec sql stmt
 type Exec interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 }
 
 // Get is select only one row
 type Get interface {
 	Get(dest interface{}, query string, args ...interface{}) error
+	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
 }
 
 // Select multiple rows
 type Select interface {
 	Select(dest interface{}, query string, args ...interface{}) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
 }
 
 // Named support named argument in stmt
 type Named interface {
 	PrepareNamed(query string) (*sqlx.NamedStmt, error)
 	NamedExec(query string, arg interface{}) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
 }
 
+// Queryx support query row
 type Queryx interface {
 	Queryx(query string, args ...interface{}) (*sqlx.Rows, error)
+	QueryxContext(ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error)
 }
 
 // Transaction for db connection
