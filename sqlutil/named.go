6b3792fe863e67a8f7a50e1ea54e409e104e6b35
tsaikd <tsaikd@gmail.com>
2018-10-16 23:12:55 +0800

    sqlutil: remove non context query, add strict function for select
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- sqlutil/named.go
+++ sqlutil/named.go
@@ -15,22 +15,6 @@ var (
 	ErrorNoRowAffected = errutil.NewFactory("no row affected")
 )
 
-// NamedExec prepare named SQL statement and call Exec function
-func NamedExec(
-	tx dbtypes.Named,
-	query string,
-	arg interface{},
-) (result sql.Result, err error) {
-	stmt, err := tx.PrepareNamed(query)
-	if err != nil {
-		return result, ErrorPrepareNamed.New(err)
-	}
-	if result, err = stmt.Exec(arg); err != nil {
-		return result, wrapError(err)
-	}
-	return result, nil
-}
-
 // NamedExecContext prepare named SQL statement and call Exec function with context
 func NamedExecContext(
 	ctx context.Context,
@@ -48,25 +32,6 @@ func NamedExecContext(
 	return result, nil
 }
 
-// NamedExecStrict like NamedExec, but result rows affected should greater than zero
-func NamedExecStrict(
-	tx dbtypes.Named,
-	query string,
-	arg interface{},
-) (result sql.Result, err error) {
-	if result, err = NamedExec(tx, query, arg); err != nil {
-		return result, err
-	}
-	num, err := result.RowsAffected()
-	if err != nil {
-		return result, err
-	}
-	if num < 1 {
-		return result, ErrorNoRowAffected.New(nil)
-	}
-	return result, nil
-}
-
 // NamedExecStrictContext like NamedExecContext, but result rows affected should greater than zero
 func NamedExecStrictContext(
 	ctx context.Context,
@@ -74,12 +39,16 @@ func NamedExecStrictContext(
 	query string,
 	arg interface{},
 ) (result sql.Result, err error) {
-	if result, err = NamedExecContext(ctx, tx, query, arg); err != nil {
-		return result, err
+	stmt, err := tx.PrepareNamed(query)
+	if err != nil {
+		return result, ErrorPrepareNamed.New(err)
+	}
+	if result, err = stmt.ExecContext(ctx, arg); err != nil {
+		return result, wrapStrictError(err)
 	}
 	num, err := result.RowsAffected()
 	if err != nil {
-		return result, err
+		return result, wrapStrictError(err)
 	}
 	if num < 1 {
 		return result, ErrorNoRowAffected.New(nil)
@@ -87,24 +56,7 @@ func NamedExecStrictContext(
 	return result, nil
 }
 
-// NamedGet prepare named SQL statement and call Get function
-func NamedGet(
-	tx dbtypes.Named,
-	dest interface{},
-	query string,
-	arg interface{},
-) (err error) {
-	stmt, err := tx.PrepareNamed(query)
-	if err != nil {
-		return ErrorPrepareNamed.New(err)
-	}
-	if err = stmt.Get(dest, arg); err != nil {
-		return wrapError(err)
-	}
-	return nil
-}
-
-// NamedGetContext prepare named SQL statement and call Get function with context
+// NamedGetContext prepare named SQL statement and call Get function with context, return error if get nothing
 func NamedGetContext(
 	ctx context.Context,
 	tx dbtypes.Named,
@@ -117,24 +69,7 @@ func NamedGetContext(
 		return ErrorPrepareNamed.New(err)
 	}
 	if err = stmt.GetContext(ctx, dest, arg); err != nil {
-		return wrapError(err)
-	}
-	return nil
-}
-
-// NamedSelect prepare named SQL statement and call Select function
-func NamedSelect(
-	tx dbtypes.Named,
-	dest interface{},
-	query string,
-	arg interface{},
-) (err error) {
-	stmt, err := tx.PrepareNamed(query)
-	if err != nil {
-		return ErrorPrepareNamed.New(err)
-	}
-	if err = stmt.Select(dest, arg); err != nil {
-		return wrapError(err)
+		return wrapStrictError(err)
 	}
 	return nil
 }
@@ -153,6 +88,24 @@ func NamedSelectContext(
 	}
 	if err = stmt.SelectContext(ctx, dest, arg); err != nil {
 		return wrapError(err)
+	}
+	return nil
+}
+
+// NamedSelectStrictContext prepare named SQL statement and call Select function with context, return error if select nothing
+func NamedSelectStrictContext(
+	ctx context.Context,
+	tx dbtypes.Named,
+	dest interface{},
+	query string,
+	arg interface{},
+) (err error) {
+	stmt, err := tx.PrepareNamed(query)
+	if err != nil {
+		return ErrorPrepareNamed.New(err)
+	}
+	if err = stmt.SelectContext(ctx, dest, arg); err != nil {
+		return wrapStrictError(err)
 	}
 	return nil
 }
@@ -182,7 +135,45 @@ func NamedSelectInContext(
 	return nil
 }
 
+// NamedSelectInStrictContext prepare named/in SQL statement and call Select function with context, return error if select nothing
+func NamedSelectInStrictContext(
+	ctx context.Context,
+	tx dbtypes.Transactionx,
+	dest interface{},
+	query string,
+	arg interface{},
+) (err error) {
+	query, args, err := sqlx.Named(query, arg)
+	if err != nil {
+		return ErrorPrepareNamed.New(err)
+	}
+
+	query, args, err = sqlx.In(query, args...)
+	if err != nil {
+		return ErrorPrepareNamed.New(err)
+	}
+
+	query = tx.Rebind(query)
+	if err = tx.SelectContext(ctx, dest, query, args...); err != nil {
+		return wrapStrictError(err)
+	}
+	return nil
+}
+
 func wrapError(err error) error {
+	if IsErrorNoRowsInResultSet(err) {
+		return nil
+	}
+	if IsErrorDuplicateViolateUniqueConstraint(err) {
+		return ErrorDuplicateViolateUniqueConstraint.New(err)
+	}
+	if IsErrorTsquerySyntax(err) {
+		return ErrorTsquerySyntax.New(err)
+	}
+	return err
+}
+
+func wrapStrictError(err error) error {
 	if IsErrorNoRowsInResultSet(err) {
 		return ErrorNoRowsInResultSet.New(err)
 	}
